b7dd24a3ad8a31b95dd73af922875c1077a2cf49
2dongsik2 <70863633+2dongsik2@users.noreply.github.com>
2021-10-01 09:37:32 +0900

    Update run.py

--- run.py
+++ run.py
@@ -138,7 +138,7 @@ for i in range(len(nums)):
   if i % 100 == 0:
     conn.commit()
     files_conn.commit()
-  if i >= 250:
+  if i >= 25000:
     break
   write_date()
   logs += data["id"] + "\n"
